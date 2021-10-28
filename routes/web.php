826f52b2288eb43208dbda7e202769451fc34ba9
Edkar Chachati <chachati28@gmail.com>
2021-10-28 16:49:45 -0400

    deploy 4th attemp probably breaking some shit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,42 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth:jwt')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::post('update', 'App\Http\Controllers\AuthController@update');
+    Route::post('update/{id}', 'App\Http\Controllers\AuthController@update');
+    Route::delete('destroy/{id}', 'App\Http\Controllers\AuthController@destroy');
+    Route::delete('destroy', 'App\Http\Controllers\AuthController@destroy');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'program'
+], function ($router) {
+    Route::get('detail/{id}', 'App\Http\Controllers\ProgramController@detail');
+    Route::get('list', 'App\Http\Controllers\ProgramController@list');
+    Route::post('create', 'App\Http\Controllers\ProgramController@create');
+    Route::post('update/{id}', 'App\Http\Controllers\ProgramController@update');
+    Route::delete('destroy/{id}', 'App\Http\Controllers\ProgramController@destroy');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'tag'
+], function ($router) {
+    Route::get('list/{name}', 'App\Http\Controllers\TagController@list');
 });
